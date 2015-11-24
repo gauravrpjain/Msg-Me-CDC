083213feadf366ebeacf07089c51f49cff10c9f2
gauravrpjain <gauravrpjain@gmail.com>
2015-11-25 03:50:09 +0530

    Parsing the CDC Noticeboard done
    Parses the notice board, finds out new updates (the ones after the last
    pushed update) and prints those.

--- src/popo/defcon/MsgMeCDC.java
+++ src/popo/defcon/MsgMeCDC.java
@@ -5,17 +5,87 @@
  */
 package popo.defcon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
 /**
  *
  * @author Popo
  */
 public class MsgMeCDC {
-
+    static String oldtime="23-11-2015 23:56";
+    String readPage(){
+     try{
+        URL site = new URL("http://cdc.iitkgp.ernet.in/notice/");
+            Proxy p = new Proxy(Proxy.Type.HTTP,new InetSocketAddress("10.3.100.207", 8080));
+            HttpURLConnection notice_board = (HttpURLConnection)site.openConnection(p);
+           // System.out.println("Proxy in Use: "+notice_board.usingProxy());
+            BufferedReader in = new BufferedReader(new InputStreamReader(notice_board.getInputStream()));
+            //System.out.println(in.toString());
+            String temp;
+            StringBuilder http = new StringBuilder();
+           // int line = 1;
+        while((temp = in.readLine()) != null){
+            http.append('\n').append(temp);
+            //System.out.println("reading line no" + line++);
+        }
+        //System.out.println(http);
+        return(http.toString());
+    }catch(Exception e){
+    e.printStackTrace();
+    return null;
+    }
+    }
+    
+    void Parse(){
+    String input = readPage();
+        if(input==null){
+        System.out.println("Error connecting to Internet");
+        return;
+    }        
+        String time;
+        Document cdc = Jsoup.parse(input);
+        Elements notices = cdc.getElementsByTag("tbody");
+        Elements alerts = notices.get(1).getElementsByTag("tr");
+        alerts.remove(0);
+        System.out.println("Current Old Time = "+oldtime);
+        for (Element node : alerts) {
+            Elements content = node.getElementsByTag("td");
+            time = content.last().text();
+            if(convertTime(time).compareTo(convertTime(oldtime))<=0) {
+                MsgMeCDC.oldtime = alerts.get(0).getElementsByTag("td").last().text();
+                return;}
+            System.out.println("Current notice time :"+convertTime(time));
+            System.out.println("Printing new alert");
+            for (Element text : content) {
+                System.out.println(text.text());
+            }
+        }
+        //System.out.println(notices.toString());
+    }
+    
+    String convertTime(String time){
+    String[] s = time.split(" ");
+    String[] date = s[0].split("-");
+    String result = date[2]+"-"+date[1]+"-"+date[0]+" "+s[1];
+    return result;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        MsgMeCDC m = new MsgMeCDC();
+        m.Parse();
+        m.Parse();
+       }
     
 }
